80b36db0832931010520c8edd5902ab063911e6b
Rodrigo Jose <digo.1903@hotmail.com>
2025-07-14 22:38:09 -0300

    atualizado package.json

--- test/login.test.js
+++ test/login.test.js
@@ -1,12 +1,13 @@
-const request = require('supertest')
-const { expect } = require('chai')
+import request from 'supertest';
+import chai from 'chai';
+const { expect } = chai;
 
 describe('Login', () => {
     describe('POST/auth/login', () => {
-        it('Deve retornar CODE 200 e mensagem "Login realizado com sucesso" quando usar credenciais válidas ', async () =>{
+        it('Deve retornar CODE 200 e mensagem "Login realizado com sucesso" quando usar credenciais válidas ', async () => {
             const resposta = await request("http://localhost:3030")
                 .post('/auth/login')
-                .set('Content-Type','application/json')
+                .set('Content-Type', 'application/json')
                 .send({
                     'username': "usuario1",
                     'password': "senha123"
@@ -14,10 +15,10 @@ describe('Login', () => {
             expect(resposta.status).to.equal(200);
             expect(resposta.body.mensagem).to.equal("Login realizado com sucesso!")
         })
-        it('Deve retornar CODE 404 e mensagem "Usuário não cadastrado." quando usar credenciais não cadastradas', async () =>{
+        it('Deve retornar CODE 404 e mensagem "Usuário não cadastrado." quando usar credenciais não cadastradas', async () => {
             const resposta = await request("http://localhost:3030")
                 .post('/auth/login')
-                .set('Content-Type','application/json')
+                .set('Content-Type', 'application/json')
                 .send({
                     'username': "usuario-nao-cadastrado",
                     'password': "senha-invalida"
@@ -25,10 +26,10 @@ describe('Login', () => {
             expect(resposta.status).to.equal(404);
             expect(resposta.body.mensagem).to.equal("Usuário não cadastrado.")
         })
-        it('Deve retornar CODE 401 e mensagem "Credenciais inválidas." quando usar credenciais inválidas', async () =>{
+        it('Deve retornar CODE 401 e mensagem "Credenciais inválidas." quando usar credenciais inválidas', async () => {
             const resposta = await request("http://localhost:3030")
                 .post('/auth/login')
-                .set('Content-Type','application/json')
+                .set('Content-Type', 'application/json')
                 .send({
                     'username': "usuario1",
                     'password': "senha-invalida"
@@ -37,24 +38,24 @@ describe('Login', () => {
             expect(resposta.body.mensagem).to.equal("Credenciais inválidas.")
         })
 
-        it('Deve retornar CODE 401 e mensagem "Usuário bloqueado por excesso de tentativas." quando usar credenciais inválidas por 3 tentativas', async () =>{
+        it('Deve retornar CODE 401 e mensagem "Usuário bloqueado por excesso de tentativas." quando usar credenciais inválidas por 3 tentativas', async () => {
             await request("http://localhost:3030")
                 .post('/auth/login')
-                .set('Content-Type','application/json')
+                .set('Content-Type', 'application/json')
                 .send({
                     'username': "usuario1",
                     'password': "senha-invalida"
                 })
             await request("http://localhost:3030")
                 .post('/auth/login')
-                .set('Content-Type','application/json')
+                .set('Content-Type', 'application/json')
                 .send({
                     'username': "usuario1",
                     'password': "senha-invalida"
-                })    
+                })
             const resposta = await request("http://localhost:3030")
                 .post('/auth/login')
-                .set('Content-Type','application/json')
+                .set('Content-Type', 'application/json')
                 .send({
                     'username': "usuario1",
                     'password': "senha-invalida"
@@ -67,20 +68,20 @@ describe('Login', () => {
     })
 
     describe('POST/auth/forgot-password', () => {
-        it('Deve retornar CODE 200 quando o lembrete de senha for enviado com sucesso', async () =>{
+        it('Deve retornar CODE 200 quando o lembrete de senha for enviado com sucesso', async () => {
             const resposta = await request("http://localhost:3030")
                 .post('/auth/forgot-password')
-                .set('Content-Type','application/json')
+                .set('Content-Type', 'application/json')
                 .send({
                     'username': "usuario1"
                 })
             expect(resposta.status).to.equal(200);
             expect(resposta.body.lembrete).to.equal("Seu lembrete de senha foi encaminhado para o email cadastrado: *******@email.com")
         })
-        it('Deve retornar CODE 404 quando o usuário não estiver cadastrado', async () =>{
+        it('Deve retornar CODE 404 quando o usuário não estiver cadastrado', async () => {
             const resposta = await request("http://localhost:3030")
                 .post('/auth/forgot-password')
-                .set('Content-Type','application/json')
+                .set('Content-Type', 'application/json')
                 .send({
                     'username': "usuario-nao-cadastrado"
                 })
