f9c7a74012706e5b13fa4ce3f8e919a7801e7adf
Rodrigo José <digo.1903@hotmail.com>
2025-08-06 13:29:24 -0300

    add cypress e automação da tela de cadastro de usuário

---
+++ web-app/tests/cypress/support/commands.js
@@ -0,0 +1,39 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+// Custom command to wait for toast messages
+Cypress.Commands.add('waitForToast', (expectedMessage, type = 'success') => {
+    cy.get('.toast').should('be.visible')
+    cy.get('.toast').should('contain', expectedMessage)
+
+    if (type === 'success') {
+        cy.get('.toast').should('have.class', 'green')
+    } else if (type === 'error') {
+        cy.get('.toast').should('have.class', 'red')
+    }
+})
+
+// Custom command to clear form fields
+Cypress.Commands.add('clearForm', () => {
+    cy.get('#username').clear()
+    cy.get('#password').clear()
+    cy.get('#email').clear()
+})
+
+// Custom command to generate test data
+Cypress.Commands.add('generateTestData', () => {
+    const { faker } = require('@faker-js/faker')
+
+    const username = faker.internet.userName().substring(0, 8)
+    const password = faker.internet.password({ min: 5, max: 8 })
+    const email = `${username}@test.com`
+
+    return { username, password, email }
+})
